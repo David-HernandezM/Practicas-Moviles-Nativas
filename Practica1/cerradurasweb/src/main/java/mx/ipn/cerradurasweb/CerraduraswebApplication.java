2554ec92a4546dcad3b686106105b0366921af03
andres-pg9 <115204261+andres-pg9@users.noreply.github.com>
2024-10-01 16:47:10 -0600

    Creación de proyecto con Spring

---
+++ Practica1/cerradurasweb/src/main/java/mx/ipn/cerradurasweb/CerraduraswebApplication.java
@@ -0,0 +1,13 @@
+package mx.ipn.cerradurasweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CerraduraswebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CerraduraswebApplication.class, args);
+	}
+
+}
